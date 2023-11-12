953e93cdf4c596cd8b79730bda08f1503b315c1f
Anton Bordachev <anton.bordachev@getmayd.com>
2023-11-12 13:55:00 +0100

    add e2e test case

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -6,30 +6,33 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { IOffer, Offer } from '../src/offer/entity/offer.entity';
 import { Repository } from 'typeorm';
 import { ingestOffersUseCaseToken, offerConverterToken, offerValidatorToken } from '../src/offer/offer.di';
-import { loggerFactoryToken } from '../src/core/logger/logger.di';
 import { OfferType } from '../src/offer/offer-type.enum';
 import { IngestOffersUseCase } from '../src/offer/use-cases/ingest-offers-use-case';
 import { OfferProviderClientMock } from './mock/offer-provider-client.mock';
 import { IOfferProviderClient } from '../src/external-providers/offer/offer-provider.client';
+import { LoggerMock } from './mock/logger.mock';
+import { loggerFactoryToken } from '../src/core/logger/logger.di';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
   let offersRepository: Repository<IOffer>;
   let offerProviderClientMock: OfferProviderClientMock;
+  let logger: LoggerMock;
 
   beforeAll(async () => {
     /**
      * @todo use test database configuration
      */
     offerProviderClientMock = new OfferProviderClientMock();
+    logger = new LoggerMock();
+    logger.log = jest.fn().mockResolvedValue(undefined);
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     })
       .overrideProvider(ingestOffersUseCaseToken)
       .useFactory({
-        inject: [offerConverterToken, offerValidatorToken, getRepositoryToken(Offer), loggerFactoryToken],
-        factory: (offerConverter, offerValidator, offerRepository, loggerFactory) => {
-          const logger = loggerFactory.create(IngestOffersUseCase.name);
+        inject: [offerConverterToken, offerValidatorToken, getRepositoryToken(Offer)],
+        factory: (offerConverter, offerValidator, offerRepository) => {
           return new IngestOffersUseCase(
             {
               [OfferType.OFFER_ONE]: offerProviderClientMock,
@@ -40,6 +43,10 @@ describe('AppController (e2e)', () => {
             logger,
           );
         },
+      })
+      .overrideProvider(loggerFactoryToken)
+      .useValue({
+        create: jest.fn().mockReturnValue(logger),
       })
       .compile();
     app = moduleFixture.createNestApplication();
@@ -60,6 +67,7 @@ describe('AppController (e2e)', () => {
       request(app.getHttpServer()).post('/ingest-offers').expect(200);
     });
 
+    // happy path
     it('should ingest offers to the storage', async () => {
       const offers = await offersRepository.find();
       expect(offers.length).toEqual(0);
@@ -101,6 +109,38 @@ describe('AppController (e2e)', () => {
         isIos: 0,
         isAndroid: 1,
       });
+    });
+
+    // unhappy path
+    it('should not ingest not valid offers', async () => {
+      const offers = await offersRepository.find();
+      expect(offers.length).toEqual(0);
+
+      const mockResponse = {
+        response: {
+          offers: [
+            {
+              offer_id: -1, // invalid field
+              offer_name: 'test offer_name',
+              offer_desc: 'test offer_desc',
+              call_to_action: 'test call_to_action',
+              offer_url: 'test offer_url',
+              image_url: 'test image_url',
+              platform: 'android',
+              device: 'test',
+            },
+          ],
+        },
+      };
+
+      offerProviderClientMock.fetchOffers = jest.fn().mockResolvedValue(mockResponse);
+      logger.warn = jest.fn().mockReturnValue(undefined);
+
+      await request(app.getHttpServer()).post('/ingest-offers');
+
+      const offersAfterIngestion = await offersRepository.find();
+      expect(offersAfterIngestion.length).toEqual(0);
+      expect(logger.warn).toHaveBeenCalledWith('Offer (offer1 -1) validation failed: externalOfferId must be a string');
     });
   });
 });
